e23e7c1d4dafe382031fbab9d46cde0802b55bc9
Oliver Aldrian <oliver.aldrian@diawa.at>
2018-10-11 15:23:39 +0200

    add Ants experiment

---
+++ Ants/FirstExperiment/Assets/Scripts/BallControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallControl : MonoBehaviour 
+{
+	public float speed;
+	public Text countText;
+	public Text WinText;
+
+	private Rigidbody rigidbody;
+	private int count;
+
+	void Start()
+	{
+		count = 0;
+		countText.text = "Count: " + count.ToString ();
+		WinText.text = "";
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			countText.text = "Count: " + count.ToString ();
+			if (count > 4) {
+				WinText.text = "wint";
+			}
+		}
+	}
+}
